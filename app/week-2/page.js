b40da28478d74868817316ad0cfe4bfa1701f532
JoshuaNThomson <joshua.thomson@edu.sait.ca>
2024-09-24 06:24:17 -0600

    week_2 assignment
    week_2 assignment finished

---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+import StudentInfo from './student-info.js';
+
+export default function Page() {
+    return (
+        <main>
+            <h1>shopping list</h1>
+            <StudentInfo/>
+        </main>
+    );
+}
+
+
+
+
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Student Information</h1>
+      <p>Name: Joshua Thomson</p>
+      <Link href="https://github.com/JoshuaNThomson/cprg306-assignments">Github repository</Link>
+    </div>
+  );
+}
